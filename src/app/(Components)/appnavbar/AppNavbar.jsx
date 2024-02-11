758c5ace145ec1dbf6e4be1fc294f4ca75de5455
Ahmad Muqadas <ahmadmuqadas.98@gmail.com>
2024-02-10 21:55:34 -0800

    fixed the extra margin bug.

--- src/app/(Components)/appnavbar/AppNavbar.jsx
+++ src/app/(Components)/appnavbar/AppNavbar.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import Image from 'next/image'
-import LogoSource from './logo.png';
+
 import userIcon from './userIcon.svg'
 
 
 const AppNavbar = () => {
   return (
-    <nav className='container m-auto nav flex justify-between items-center px-3 h-20 bg-blue-500 lg:px-20'>
+    <nav className=  'm-auto nav flex justify-between items-center px-3 h-20 bg-blue-500 lg:px-20 max-w-screen-2xl'>
       <div className=""><p className='font-bold text-white text-2xl '>Caltracker</p></div>
       <div className="">
       <Image className='w-10'
