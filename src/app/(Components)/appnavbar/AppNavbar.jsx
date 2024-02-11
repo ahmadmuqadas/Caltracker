519a321ed2b7c7756b030923e4d48dbdb976669d
Ahmad Muqadas <ahmadmuqadas.98@gmail.com>
2024-02-10 22:07:39 -0800

    last commit before adding auth UI.

--- src/app/(Components)/appnavbar/AppNavbar.jsx
+++ src/app/(Components)/appnavbar/AppNavbar.jsx
@@ -9,9 +9,10 @@ const AppNavbar = () => {
     <nav className=  'm-auto nav flex justify-between items-center px-3 h-20 bg-blue-500 lg:px-20 max-w-screen-2xl'>
       <div className=""><p className='font-bold text-white text-2xl '>Caltracker</p></div>
       <div className="">
-      <Image className='w-10'
+      <Image className='w-10 cursor-pointer'
       src={userIcon}
       alt="Cal Tracker Logo"
+      role='button'
     />
         </div>
     </nav>
--- src/app/(Components)/hero/Hero.jsx
+++ src/app/(Components)/hero/Hero.jsx
@@ -9,9 +9,9 @@ const Hero = () => {
     <main className=' container flex flex-col items-center justify-center lg:flex-row lg:justify-between lg:items-start lg:pt-20 lg:m-auto max-w-screen-2xl'>
       <div className='px-3 pt-10 flex flex-col items-center lg:pl-20 lg:items-start lg:flex-1'>
         <h4  className='font-semibold text-center '>Best Calorie Tracker Platform</h4>
-        <h1 className='font-semibold text-center text-4xl lg:text-left'>Track your Calorie consumption and burn<span className='font-bold text-orange-400'> with CalTracker</span></h1>
+        <h1 className='font-semibold text-center text-6xl lg:text-left'>Track your Calorie consumption and burn<span className='font-bold text-orange-400'> with CalTracker</span></h1>
         <h2 className='text-center pt-4'>The best motivational app to achieve your dream Body make new Habits and More!</h2>
-        <button className=' flex gap-1 p-2 mt-10 px-10 bg-purple-900 text-white shadow-lg shadow-slate-400'>Track Today!
+        <button className=' flex gap-1 p-2 mt-10 px-10 bg-purple-900 text-white shadow-lg shadow-slate-400'>Track For Free!
         <Image
         src={Arrow}
         alt='cal tracker avatar image'
